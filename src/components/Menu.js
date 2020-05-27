8c3fd0a5125031f0962a66667744317c8a24c1a6
Nick Chen <nickchen@usc.edu>
2020-05-26 20:16:53 -0500

    dropdown menus for each category

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,41 +2,47 @@ import React, { Component } from 'react'
 import MenuCategory from './MenuCategory';
 import PropTypes from 'prop-types'
 import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 class Menu extends Component{
-
   render() {
     console.log(this.props.menu);
     console.log(Object.keys(this.props.menu));
     return (
       <div>
-        
-
-
         <h1>Menu</h1>
         {
           Object.keys(this.props.menu).map((menuCategory, menuIndex) => 
-          <Container>
-              <h2>{menuCategory}</h2>
-              <div className="categorycontainer" style={categorycontainerstyle}>
-              <Grid
-                container
-                direction="row"
-                justify="flex-start"
-                alignItems="flex-start"
-                flexwrap="wrap"
-                spacing={2}>
-                <MenuCategory menuCategory = {this.props.menu[menuCategory]}/>
-              </Grid>
-            </div>
-          </Container>
+          <Paper>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <h2>{menuCategory}</h2>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Grid
+                  container
+                  direction="row"
+                  justify="flex-start"
+                  alignItems="flex-start"
+                  flexwrap="wrap"
+                  spacing={2}
+                >
+                  <MenuCategory menuCategory = {this.props.menu[menuCategory]}/>
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </Paper>
           )
         }
       </div>
