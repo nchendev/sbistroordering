ada520a82b6cec664847c9910d8daceb667f2105
Nick Chen <nickchen@usc.edu>
2020-06-04 02:55:25 -0500

    rebuilt sources, merged branches

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ if (process.env.NODE_ENV === 'production') {
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
+} else {
+	// set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 // catch 404 and forward to error handler
