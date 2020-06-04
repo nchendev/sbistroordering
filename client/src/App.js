d79d430fa40d428cf3406e29d2f0cff916e9c476
Nick Chen <nickchen@usc.edu>
2020-06-04 02:06:08 -0500

    http -> https

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
 		// call API
 		console.log('calling api');
 		axios
-			.post('http://sbordering.herokuapp.com/twilio', orderJSON)
+			.post('https://sbordering.herokuapp.com/twilio', orderJSON)
 			.then((res) => console.log(res.data))
 			.catch((err) => {
 				console.error(err);
