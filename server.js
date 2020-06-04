bffca882b5fbd0d14f802dd2800f5826d7b47797
Nick Chen <nickchen@usc.edu>
2020-06-04 01:25:07 -0500

    removing node_modules references

--- server.js
+++ server.js
@@ -28,10 +28,13 @@ app.use(cors());
 app.use('/twilio', require('./routes/twilio'));
 
 // Client routing
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
